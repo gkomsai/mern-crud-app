11b475ccbc4cab20e96c86035b367ae0ab0206fe
gkomsai <gkomsai788@gmail.com>
2022-11-14 23:53:21 +0530

    email and password validator added

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -5,88 +5,98 @@ const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const { checkUserAuth } = require("../middleware/authMiddleware.js");
 const { UserModel } = require("../models/userModel.js");
+const { emailValidator } = require("../middleware/emailValidator.js");
+const { passwordValidator } = require("../middleware/PasswordValidator.js");
 
 const userRouter = Router();
 
 /* ------------------ SignUp Logic----------------------------------- */
 
-userRouter.post("/signup", async (req, res) => {
-  const { email, password } = req.body;
-  const isEmailPresent = await UserModel.findOne({ email });
-  // console.log(isEmailPresent);
-  if (isEmailPresent) {
-    res.send("already signUp");
-  } else {
-    bcrypt
-      .hash(password, 10)
-      .then(async function (hash) {
-        const newUser = new UserModel({ ...req.body, password: hash });
-        await newUser.save();
-        return res
-          .status(201)
-          .json({
-            message: "successfully signUp in the database",
+userRouter.post(
+  "/signup",
+  [emailValidator, passwordValidator],
+  async (req, res) => {
+    const { email, password } = req.body;
+    const isEmailPresent = await UserModel.findOne({ email });
+    // console.log(isEmailPresent);
+    if (isEmailPresent) {
+      return res
+        .status(400)
+        .send({ status: "error", message: "Email already exists" });
+    } else {
+      bcrypt
+        .hash(password, 10)
+        .then(async function (hash) {
+          const newUser = new UserModel({ ...req.body, password: hash });
+          await newUser.save();
+          return res.status(201).json({
+            message: "SignUp Successfull!!!",
             status: "success",
             user: newUser,
           });
-      })
-      .catch((err) => {
-        res.status(400).send({status:"error", message: err.message});
-      });
-  }
-});
-
-userRouter.post("/login", async (req, res) => {
-  try {
-    const { email, password } = req.body;
-    if (email && password) {
-      const user = await UserModel.findOne({ email });
-      //   console.log(user);
-      if (user) {
-        let hash = user.password;
-        bcrypt.compare(password, hash, function (err, result) {
-          if (err) {
-            return res.send({
-              message: "Something went wrong, plz try again later",
-              status: "error",
-            });
-          }
-          if (result) {
-            const token = jwt.sign(
-              { userId: user._id, email: email },
-              process.env.JWT_SECRET_KEY,
-              {
-                expiresIn: "5h",
-              }
-            );
-            return res.status(200).send({
-              status: "success",
-              message: "Login Successfull!!!",
-              token: token,
-              user: user,
-            });
-          } else {
-            return res
-              .status(400)
-              .send({ status: "error", message: "Invalid Credentials" });
-          }
+        })
+        .catch((err) => {
+          res.status(400).send({ status: "error", message: err.message });
         });
+    }
+  }
+);
+
+
+
+userRouter.post(
+  "/login",
+  [emailValidator, passwordValidator],
+  async (req, res) => {
+    try {
+      const { email, password } = req.body;
+      if (email && password) {
+        const user = await UserModel.findOne({ email });
+        //   console.log(user);
+        if (user) {
+          let hash = user.password;
+          bcrypt.compare(password, hash, function (err, result) {
+            if (err) {
+              return res.send({
+                message: "Something went wrong, plz try again later",
+                status: "error",
+              });
+            }
+            if (result) {
+              const token = jwt.sign(
+                { userId: user._id, email: email },
+                process.env.JWT_SECRET_KEY,
+                {
+                  expiresIn: "5h",
+                }
+              );
+              return res.status(200).send({
+                status: "success",
+                message: "Login Successfull!!!",
+                token: token,
+                user: user,
+              });
+            } else {
+              return res
+                .status(400)
+                .send({ status: "error", message: "Invalid Credentials" });
+            }
+          });
+        } else {
+          return res
+            .status(400)
+            .send({ status: "error", message: "Invalid Credentials" });
+        }
       } else {
         return res
           .status(400)
-          .send({ status: "error", message: "Invalid Credentials" });
+          .send({ status: "error", message: "All Fields are Required" });
       }
-    } else {
-      return res
-        .status(400)
-        .send({ status: "error", message: "All Fields are Required" });
+    } catch (err) {
+      // console.log(err);
+      return res.status(400).send({ status: "error", message: err.message });
     }
-  } catch (err) {
-    // console.log(err);
-    return res
-      .status(400)
-      .send({ status: "error", message: err.message });
   }
-});
+);
 
 module.exports = { userRouter };
