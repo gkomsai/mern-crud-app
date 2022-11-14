a96f6e3f66db70554c767270f5ed93e0fc0678c2
gkomsai <gkomsai788@gmail.com>
2022-11-15 00:20:45 +0530

    all backend code is structured as per the MVC model

---
+++ server/controller/albumController.js
@@ -0,0 +1,108 @@
+
+const getAllAlbums = async (req, res) => {
+    try {
+      // console.log("query", req.query);
+      let { _sort, _order, page, limit, ...rest } = req.query;
+      page = +page || 1;
+      limit = +limit || 6;
+      const skip = (page - 1) * limit;
+      let order;
+      if (_order) {
+        order = _order === "asc" ? 1 : -1;
+      }
+      const totalAlbum = await AlbumModel.find({}).count();
+      const totalPages = Math.ceil(totalAlbum / limit);
+      if (req.query) {
+        // console.log("inside this")
+        const result = await AlbumModel.find(rest)
+          .skip(skip)
+          .limit(limit)
+          .sort({ year: order });
+        return res.status(200).send({ result, totalPages });
+      } else {
+        const result = await AlbumModel.find();
+        console.log("else");
+        if (result.length > 0) {
+          return res.status(200).send({ result, totalPages });
+        } else {
+          return res
+            .status(400)
+            .json({ message: "please create a album first", status: "error" });
+        }
+      }
+    } catch (error) {
+      console.log({ error });
+      res.status(400).json({ message: "Something went wrong", status: "error" });
+    }
+}
+  
+
+  
+const getOneAlbum = async (req, res) => {
+    try {
+      const id = req.params.id;
+      const result = await AlbumModel.findById({id});
+      return res.status(200).send(result);
+    } catch (err) {
+      return res.status(500).json({ message: err.message, status: "error" });
+    }
+}
+  
+const addAlbum =  async (req, res) => {
+    try {
+      const newAlbum = new AlbumModel(req.body);
+      await newAlbum.save();
+      return res.status(201).send(newAlbum);
+    } catch (err) {
+      return res.status(500).json({ message: err.message, status: "error" });
+    }
+}
+
+
+const updateAlbum = async (req, res) => {
+    try {
+      const id = req.params.id;
+      const { userId } = req.body;
+      if (id === userId) {
+      const updatedAlbum = await AlbumModel.findByIdAndUpdate({id}, req.body, { new: true, });
+          if (updatedAlbum) {
+          return res
+            .status(201)
+            .send({ status: "success", message: " updated successfully" });
+        }
+      } else {
+        return res.status(400).send({
+          status: "error",
+          message: "you are not authorize to update this album",
+        });
+      }
+    } catch (err) {
+      return res.status(500).json({ message: err.message, status: "error" });
+    }
+}
+  
+
+const deleteAlbum = async (req, res) => {
+    try {
+      const id = req.params.id;
+      const { userId } = req.body;
+      if (id === userId) {
+        const deleteAlbum = await AlbumModel.findByIdAndDelete({id});
+  
+        if (deleteAlbum) {
+          return res
+            .status(200)
+            .send({ status: "success", message: "Album successfully deleted" });
+        }
+      } else {
+        return res.status(400).send({
+          status: "error",
+          message: "you are not authorize to delete this album",
+        });
+      }
+    } catch (err) {
+      return res.status(500).json({ message: err.message, status: "error" });
+    }
+}
+  
+module.exports= {getAllAlbums,getOneAlbum,addAlbum,updateAlbum,deleteAlbum}
--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -3,100 +3,22 @@ const { Router } = require("express");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
-const { checkUserAuth } = require("../middleware/authMiddleware.js");
+
+
 const { UserModel } = require("../models/userModel.js");
 const { emailValidator } = require("../middleware/emailValidator.js");
-const { passwordValidator } = require("../middleware/PasswordValidator.js");
+const { passwordValidator } = require("../middleware/passwordValidator.js");
+const {loginController,signupController} = require("../controller/userController.js");
+
 
 const userRouter = Router();
 
-/* ------------------ SignUp Logic----------------------------------- */
-
-userRouter.post(
-  "/signup",
-  [emailValidator, passwordValidator],
-  async (req, res) => {
-    const { email, password } = req.body;
-    const isEmailPresent = await UserModel.findOne({ email });
-    // console.log(isEmailPresent);
-    if (isEmailPresent) {
-      return res
-        .status(400)
-        .send({ status: "error", message: "Email already exists" });
-    } else {
-      bcrypt
-        .hash(password, 10)
-        .then(async function (hash) {
-          const newUser = new UserModel({ ...req.body, password: hash });
-          await newUser.save();
-          return res.status(201).json({
-            message: "SignUp Successfull!!!",
-            status: "success",
-            user: newUser,
-          });
-        })
-        .catch((err) => {
-          res.status(400).send({ status: "error", message: err.message });
-        });
-    }
-  }
-);
 
 
+userRouter.post("/signup",[emailValidator, passwordValidator], signupController);
 
-userRouter.post(
-  "/login",
-  [emailValidator, passwordValidator],
-  async (req, res) => {
-    try {
-      const { email, password } = req.body;
-      if (email && password) {
-        const user = await UserModel.findOne({ email });
-        //   console.log(user);
-        if (user) {
-          let hash = user.password;
-          bcrypt.compare(password, hash, function (err, result) {
-            if (err) {
-              return res.send({
-                message: "Something went wrong, plz try again later",
-                status: "error",
-              });
-            }
-            if (result) {
-              const token = jwt.sign(
-                { userId: user._id, email: email },
-                process.env.JWT_SECRET_KEY,
-                {
-                  expiresIn: "5h",
-                }
-              );
-              return res.status(200).send({
-                status: "success",
-                message: "Login Successfull!!!",
-                token: token,
-                user: user,
-              });
-            } else {
-              return res
-                .status(400)
-                .send({ status: "error", message: "Invalid Credentials" });
-            }
-          });
-        } else {
-          return res
-            .status(400)
-            .send({ status: "error", message: "Invalid Credentials" });
-        }
-      } else {
-        return res
-          .status(400)
-          .send({ status: "error", message: "All Fields are Required" });
-      }
-    } catch (err) {
-      // console.log(err);
-      return res.status(400).send({ status: "error", message: err.message });
-    }
-  }
-);
+userRouter.post("/login", [emailValidator, passwordValidator], loginController);
+
+
 
 module.exports = { userRouter };
